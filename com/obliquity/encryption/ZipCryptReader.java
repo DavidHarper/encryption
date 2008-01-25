66810d277dcee8a68eae7e6b8708fca42366b978
David Harper <david@obliquity.net>
2008-01-25 09:09:55 +0000

    Changed default unpacking behaviour.  The zip files will be unpacked unless the user specifies -Dunpack=false.
    git-svn-id: file:///tmp/svn/trunk/encryption@584 1e52a155-5105-4563-9ffd-6b3ed74ea100

--- com/obliquity/encryption/ZipCryptReader.java
+++ com/obliquity/encryption/ZipCryptReader.java
@@ -124,8 +124,11 @@ public class ZipCryptReader {
 		return totlen;
 	}
 
+	private static final String UNPACK_PROPERTY_NAME = "unpack";
+	
 	public static void main(String[] args) {
-		boolean unpack = Boolean.getBoolean("unpack");
+		boolean unpack = (System.getProperty(UNPACK_PROPERTY_NAME) == null) ?
+				true : Boolean.getBoolean(UNPACK_PROPERTY_NAME);
 
 		try {
 			File[] files = null;
